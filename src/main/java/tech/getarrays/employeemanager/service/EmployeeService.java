8cd1076e840f14d032367434243dd86a64574754
Niyatha Rangarajan <rangaran@mcmaster.ca>
2022-07-09 12:26:43 -0400

    Adding employee service

---
+++ src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
@@ -0,0 +1,20 @@
+package tech.getarrays.employeemanager.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import tech.getarrays.employeemanager.model.Employee;
+import tech.getarrays.employeemanager.repo.EmployeeRepo;
+
+import java.util.UUID;
+
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo){
+        this.employeeRepo = employeeRepo;
+    }
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+}
