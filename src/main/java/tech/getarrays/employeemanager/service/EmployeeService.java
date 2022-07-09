e91d60b23cba11a6359fd3c99d3e6ac7fe377c86
Niyatha Rangarajan <rangaran@mcmaster.ca>
2022-07-09 14:12:06 -0400

    Adding http requests

--- src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
+++ src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
@@ -1,9 +1,11 @@
 package tech.getarrays.employeemanager.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import tech.getarrays.employeemanager.exception.UserNotFoundException;
 import tech.getarrays.employeemanager.model.Employee;
 import tech.getarrays.employeemanager.repo.EmployeeRepo;
 
+import java.util.List;
 import java.util.UUID;
 
 public class EmployeeService {
@@ -17,4 +19,19 @@ public class EmployeeService {
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepo.save(employee);
     }
+
+    public List<Employee> findAllEmployees(){
+        return employeeRepo.findAll();
+    }
+public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+}
+
+public Employee findEmployeeById(Long id){
+    return employeeRepo.findEmployeeById(id).orElseThrow(() -> new UserNotFoundException("User by id"+id+"was not found"));
+}
+public void deleteEmployee(Long id){
+        employeeRepo.deleteEmployeeById(id);
+}
+
 }
